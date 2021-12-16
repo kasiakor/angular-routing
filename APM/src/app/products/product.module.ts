f8b3c91559323bb573ee784afd20b5edf5964c86
kasia.kordys <kasia.kordys@gmail.com>
2021-12-16 22:53:35 +0200

    configure child routes for product edit page

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -4,6 +4,9 @@ import { ProductResolver } from './product-resolver.service';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
+
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -18,13 +21,27 @@ import { SharedModule } from '../shared/shared.module';
        resolve: { resolvedData: ProductResolver } },
       { path: 'products/:id/edit',
        component: ProductEditComponent,
-       resolve: { resolvedData: ProductResolver } }
+       resolve: { resolvedData: ProductResolver },
+       children: [
+        {
+          path: '', redirectTo: 'info'
+        },
+        { 
+          path: 'info', component: ProductEditInfoComponent
+        },
+        { 
+          path: 'tag', component: ProductEditTagsComponent
+        }
+        
+      ] }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
