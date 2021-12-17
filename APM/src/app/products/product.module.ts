ce3495027664f3e2616a9382204dfed07a939e6f
kasia.kordys <kasia.kordys@gmail.com>
2021-12-17 20:39:03 +0200

    group routes for products

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -15,25 +15,28 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',
-       component: ProductDetailComponent,
-       resolve: { resolvedData: ProductResolver } },
-      { path: 'products/:id/edit',
-       component: ProductEditComponent,
-       resolve: { resolvedData: ProductResolver },
-       children: [
-        {
-          path: '', redirectTo: 'info', pathMatch:'full'
-        },
-        { 
-          path: 'info', component: ProductEditInfoComponent
-        },
-        { 
-          path: 'tags', component: ProductEditTagsComponent
-        }
-        
-      ] }
+      { path: 'products',
+        component: ProductListComponent,
+        children: [
+          { path: ':id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver } },
+        { path: ':id/edit',
+          component: ProductEditComponent,
+        resolve: { resolvedData: ProductResolver },
+        children: [
+          {
+            path: '', redirectTo: 'info', pathMatch:'full'
+          },
+          { 
+            path: 'info', component: ProductEditInfoComponent
+          },
+          { 
+            path: 'tags', component: ProductEditTagsComponent
+          }
+          ]   
+        }, 
+      ]}
     ])
   ],
   declarations: [
